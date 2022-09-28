23ee14c524915d2aa5eaa7bb012ca0832da67cea
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-28 12:36:41 -0500

    user

--- create.sh
+++ create.sh
@@ -22,7 +22,7 @@ create_ec2() {
 }
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-AMI_ID=ami-0d4e7e7bed8fa2161
+AMI_ID=ami-0da08e4e5b8943a45
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
