2c017ec92520f1d7194f9b0e4bbb8f21a4adad70
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-09-28 14:01:35 -0500

    user

--- create.sh
+++ create.sh
@@ -21,8 +21,7 @@ create_ec2() {
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
 
-#AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-AMI_ID=ami-0da08e4e5b8943a45
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
