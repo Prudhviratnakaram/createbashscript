eab64eff9009bf495cd6e127ed54f7fc3884e27a
Prudhviratnakaram <ratnakaramprudhvi@gmail.com>
2022-10-16 12:12:29 -0500

    user

--- createsinglenode.sh
+++ createsinglenode.sh
@@ -13,7 +13,7 @@ create_ec2() {
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
-      --iam-instance-profile Name=SecretManager_Role_for_RoboShop_Nodes \
+      --iam-instance-profile Name=aws-cli-workstation \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 }
